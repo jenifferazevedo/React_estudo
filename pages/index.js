db7431c2f33661c0788e66ad40540d170d4be064
Jeniffer Q. F. R. de Azevedo <jeniqf@gmail.com>
2019-07-17 12:20:23 -0300

    Treino

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Layout } from 'antd';
 
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function index() {
     return (
         <div>
-            
+            <Layout>
+                <Header>Menu</Header>
+            </Layout>
         </div>
     )
 }
